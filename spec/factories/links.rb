ad9a6c5d6d9039771e0027a985ef21b9c0ff4dfb
PapaKramb <guccigang5554@gmail.com>
2021-04-21 04:02:32 +0900

    Fix

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :link do
-    sequence(:name) { |n| "Link#{n}" }
+    sequence(:title) { |n| "Link#{n}" }
     url { 'http://google.com' }
     linkable factory: :question
   end
